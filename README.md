22ff2a73f4cdac2817772ae431182c712b9bf4bf
unknown <tusharshorma471@gmail.com>
2022-06-15 16:21:21 -0400

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# testproject
+My take home project for Clover.
+Encountered some issues with filter system.
+Wanted to work with more of the architecture of the project and its scope.
+Had in mind of a six componet react app and its features
---
+++ cloverproject/src/App.js
@@ -0,0 +1,83 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
+import React, { useState, useEffect } from "react";
+
+import Search from "./components/search/search";
+import Card from "./components/cards/card";
+import Pagination from "./components/pagination/pagination";
+import Filter from "./components/filters/filters";
+import Navbar from "./components/navbar/navbar";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Episodes from "./pages/episodes";
+import Location from "./pages/location";
+import CardDetails from "./components/cards/cardDetails";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:id" element={<CardDetails />} />
+
+        <Route path="/episodes" element={<Episodes />} />
+        <Route path="/episodes/:id" element={<CardDetails />} />
+
+        <Route path="/location" element={<Location />} />
+        <Route path="/location/:id" element={<CardDetails />} />
+      </Routes>
+    </Router>
+  );
+}
+
+const Home = () => {
+  let [pageNumber, updatePageNumber] = useState(1);
+  let [status, updateStatus] = useState("");
+  let [gender, updateGender] = useState("");
+  let [species, updateSpecies] = useState("");
+  let [fetchedData, updateFetchedData] = useState([]);
+  let [search, setSearch] = useState("");
+  let { info, results } = fetchedData;
+
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&status=${status}&gender=${gender}&species=${species}`;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      updateFetchedData(data);
+    })();
+  }, [api]);
+  return (
+    <div className="App">
+      <h1 className="text-center mb-3">Characters</h1>
+      <Search setSearch={setSearch} updatePageNumber={updatePageNumber} />
+      <div className="container">
+        <div className="row">
+          <Filter
+            pageNumber={pageNumber}
+            status={status}
+            updateStatus={updateStatus}
+            updateGender={updateGender}
+            updateSpecies={updateSpecies}
+            updatePageNumber={updatePageNumber}
+          />
+          <div className="col-lg-8 col-12">
+            <div className="row">
+              <Card page="/" results={results} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Pagination
+        info={info}
+        pageNumber={pageNumber}
+        updatePageNumber={updatePageNumber}
+      />
+    </div>
+  );
+};
+
+export default App;
